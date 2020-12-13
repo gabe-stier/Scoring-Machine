dd86491c8208363265f86ff7d67a4231c50876ab
Gabe <gabezter4@yahoo.com>
2020-12-13 17:06:54 -0600

    Front End Stuff

--- front_end/utilities.py
+++ front_end/utilities.py
@@ -5,6 +5,10 @@ Created on Nov 26, 2020
 '''
 
 import logging
+from hashlib import blake2b
+import os
+from datetime import datetime
+
 
 class Loggers:
     '''
@@ -15,3 +19,18 @@ class Loggers:
     Web = logging.getLogger("web")
     Error = logging.getLogger('error')
 
+
+class Token:
+    token = 'NULL'
+
+    def __init__(self):
+        if os.path.exists('/tmp/scoring_token'):
+            with open('/tmp/scoring_token', 'r') as f:
+                self.token = f.read()
+        else:
+            with open('/tmp/scoring_token', 'w') as f:
+                m = blake2b(salt='semo')
+                m.update(datetime.now().encode())
+                token = m.hexdigest()
+                f.write(token)
+                self.token = token
