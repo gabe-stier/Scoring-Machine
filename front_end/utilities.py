98067a214242b833370a88b02570dd38904164b5
Gabe Stier <37159135+gabe-stier@users.noreply.github.com>
2020-12-14 11:55:46 -0600

    Revert "Front End Stuff"
    This reverts commit dd86491c8208363265f86ff7d67a4231c50876ab.

--- front_end/utilities.py
+++ front_end/utilities.py
@@ -5,10 +5,6 @@ Created on Nov 26, 2020
 '''
 
 import logging
-from hashlib import blake2b
-import os
-from datetime import datetime
-
 
 class Loggers:
     '''
@@ -19,18 +15,3 @@ class Loggers:
     Web = logging.getLogger("web")
     Error = logging.getLogger('error')
 
-
-class Token:
-    token = 'NULL'
-
-    def __init__(self):
-        if os.path.exists('/tmp/scoring_token'):
-            with open('/tmp/scoring_token', 'r') as f:
-                self.token = f.read()
-        else:
-            with open('/tmp/scoring_token', 'w') as f:
-                m = blake2b(salt='semo')
-                m.update(datetime.now().encode())
-                token = m.hexdigest()
-                f.write(token)
-                self.token = token
