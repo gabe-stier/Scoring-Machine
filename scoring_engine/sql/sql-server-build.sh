5623c954d22ab0b1c8f83fba070f4821d15bc0a7
Gabe <gabezter4@yahoo.com>
2020-12-31 19:18:57 -0600

    Update

--- scoring_engine/sql/sql-server-build.sh
+++
@@ -1,6 +0,0 @@
-docker run --name mysql \
-            -p 3306:3306 \
-            -e MYSQL_ROOT_PASSWORD='123456' \
-            -e MYSQL_USER='scoring_engine' \
-            -e MYSQL_PASSWORD='Changeme1!' \
-            -d mysql:latest
