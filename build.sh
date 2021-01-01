4cba6944a1ce44316611a0d9a9af1b1db81627b4
Gabe <gabezter4@yahoo.com>
2020-12-31 18:30:17 -0600

    Cleaning up workspace

--- build.sh
+++
@@ -1,7 +0,0 @@
-docker stop $(docker ps -a -q -f since=mysql); 
-count=$(docker ps -a -q -f since=mysql | wc -l)
-if (($count >= 5)); then
-    echo "Before starting. Cleaning up workbench"
-    docker system prune -f 
-fi
-docker build . -t scoring; docker run -dp 80:5000 -p 5001:5001 scoring:latest
