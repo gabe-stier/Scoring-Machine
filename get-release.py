8ed1ddb819e277c8573fb80f463a59baffcf3809
Gabe Stier <gabezter4@yahoo.com>
2021-01-05 22:05:09 -0600

    New workflow design

---
+++ get-release.py
@@ -0,0 +1,21 @@
+import requests
+import sys
+
+url = "https://api.github.com/repos/gabe-stier/Scoring-Machine/git/refs/tags"
+
+payload={}
+headers = {
+  'Accept': 'application/vnd.github.v3+json',
+  'Authorization': f'token {sys.argv[1]}'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+val = response.json()
+tags = []
+for ref in val:
+	tag = ref['ref']
+	if not 'latest' in tag:
+		tags.append(tag.split('/')[2])
+top_tag = max(tags)
+print(top_tag)
